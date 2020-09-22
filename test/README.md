9407c59ebbeb8a04881146ca20c9e8d0e3683d62
thalsi <thalseehthalsi609@gmail.com>
2020-09-22 10:15:00 +0530

    new poject

--- test/README.md
+++ test/README.md
@@ -1,4 +1,4 @@
-# FastApplication
+# Test
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.2.
 
--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TitleComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
