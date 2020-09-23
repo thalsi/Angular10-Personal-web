6e36c4da4403ac40993207fdf646dba54c742545
thalsi <thalseehthalsi609@gmail.com>
2020-09-23 14:41:54 +0530

    this are new project

--- shop/src/app/app.component.spec.ts
+++ shop/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test'`, () => {
+  it(`should have as title 'shop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test');
+    expect(app.title).toEqual('shop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('shop app is running!');
   });
 });
